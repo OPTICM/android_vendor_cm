67b6682a7df88077d6ec34a59c9ccde853091906
thdDude <afganidude@gmail.com>
2013-11-17 10:51:33 -0600

    Update common.mk

--- config/common.mk
+++ config/common.mk
@@ -302,6 +302,7 @@ endif
 
 PRODUCT_PROPERTY_OVERRIDES += \
   ro.cm.version=$(CM_VERSION) \
+  ro.modversion=$(CM_VERSION) \
   ro.goo.developerid=thddude \
   ro.goo.rom=CM11-Linaro \
   ro.goo.version=$(shell date -u +%Y%m%d)
